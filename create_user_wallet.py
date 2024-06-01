75825790f957c828af850b3211d3730dcbb8afb8
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2024-06-01 18:54:20 +0200

    Update and rename create_admin_wallet.py to create_user_wallet.py

--- create_user_wallet.py
+++ create_user_wallet.py
@@ -8,26 +8,21 @@ import time
 import shutil
 from functions import hash_wallet, verify_script_integrity_path, is_hex
 from dotenv import load_dotenv
-from wallet_management import save_context, save_encrypted_balance, save_salt, generate_salt, encrypt_balance, create_contexts, wallet_exists,send_wallet_hash_to_server 
+from wallet_management import save_context, save_encrypted_balance, save_salt, generate_salt, encrypt_balance, create_contexts, wallet_exists, send_wallet_hash_to_server
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 load_dotenv()
-EXPECTED_HASH = os.getenv('EXPECTED_HASH_CREATE_ADMIN_WALLET')
+EXPECTED_HASH = os.getenv('EXPECTED_HASH_CREATE_USER_WALLET')
 BASE_DIRECTORY = os.getenv('BASE_DIRECTORY')
-ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 
-if  not is_hex(EXPECTED_HASH):
+if not is_hex(EXPECTED_HASH):
     raise ValueError('Invalid expected hash')
 
-file_path = os.path.join(BASE_DIRECTORY, 'create_admin_wallet.py')
+file_path = os.path.join(BASE_DIRECTORY, 'create_user_wallet.py')
 verify_script_integrity_path(EXPECTED_HASH, file_path)
-
-def create_admin_wallet(node_id, password):
-    if password != ADMIN_PASSWORD:
-        logging.error("Invalid admin password.")
-        return False
-
+#send to server hash script
+def create_user_wallet(node_id):
     base_directory = os.path.join(BASE_DIRECTORY, 'secure', node_id)
     proof_directory = os.path.join(base_directory, 'Proof')
 
@@ -40,12 +35,12 @@ def create_admin_wallet(node_id, password):
         os.makedirs(proof_directory, exist_ok=True)
 
         context = create_contexts()
-        initial_balance = 1000000000000
+        initial_balance = 0
         encrypted_balance = encrypt_balance(context, initial_balance)
         salt = generate_salt()
 
         logging.info(f"Saving context, encrypted balance, and salt for node {node_id}")
-        
+
         if not (save_context(context, base_directory, node_id) and 
                 save_encrypted_balance(encrypted_balance, base_directory, node_id) and 
                 save_salt(salt, base_directory, node_id)):
@@ -58,7 +53,7 @@ def create_admin_wallet(node_id, password):
                 break
             time.sleep(1)
         else:
-            logging.error(f"Waiting for Wallet file tob be created: {wallet_path} does not exist.")
+            logging.error(f"Waiting for Wallet file to be created: {wallet_path} does not exist.")
             return False
 
         balance_hash = hash_wallet(base_directory, node_id)
@@ -68,9 +63,13 @@ def create_admin_wallet(node_id, password):
 
         save_balance_hash(balance_hash, base_directory, node_id)
 
-        if not send_wallet_hash_to_server(node_id, balance_hash):
-            logging.error("Failed to send wallet hash to server.")
-            return False
+        try:
+            if not send_wallet_hash_to_server(node_id, balance_hash):
+                logging.error("Failed to send wallet hash to server.")
+            else:
+                logging.info(f"Wallet hash for node {node_id} sent to server successfully.")
+        except Exception as e:
+            logging.error(f"Exception occurred while sending wallet hash to server: {e}")
 
         logging.info(f"New wallet for node {node_id} created successfully with balance hash {balance_hash}.")
         return True
@@ -85,14 +84,13 @@ def save_balance_hash(balance_hash, base_directory, node_id):
     logging.info(f"Balance hash saved to {hash_file_path}.")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Create an admin wallet.')
+    parser = argparse.ArgumentParser(description='Create a user wallet.')
     parser.add_argument('--node_id', required=True, help='The node ID for the wallet.')
-    parser.add_argument('--password', required=True, help='The admin password.')
 
     args = parser.parse_args()
 
-    success = create_admin_wallet(args.node_id, args.password)
+    success = create_user_wallet(args.node_id)
     if success:
-        print("Admin wallet created successfully.")
+        print("User wallet created successfully.")
     else:
-        print("Failed to create admin wallet.")
+        print("Failed to create user wallet.")
